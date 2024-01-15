9805df8b33763fcd0e7e052357add0487f83b5c2
John <88572943+Johnaoe@users.noreply.github.com>
2024-01-15 22:12:02 +0200

    fix contentSecurityPolicy

--- myApp.js
+++ myApp.js
@@ -57,7 +57,7 @@ app.use(helmet.noCache())
 app.use(helmet.contentSecurityPolicy({
   directives: {
   defaultSrc: ["'self'"],
-  srciptSrc: ["'self'", 'trusted-cdn.com']
+  scriptSrc: ["'self'", 'trusted-cdn.com']
   },
 })
 );
